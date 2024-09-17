beb37fafa2550ba0e7bd4bb36c49ff82d7ba7cd0
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2024-09-18 01:05:13 +0500

    Update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Tariq Mehmood\.1.All Done Projects\Python\django\APIs\01_rest_framework\venv
+set VIRTUAL_ENV=C:\Users\Tariq Mehmood\.1.All Done Projects\Python\django\APIs\01_rest_framework\django-api\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
